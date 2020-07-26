37490cbb1ed6b068ca570fa365cee975adf3a7f5
Vladimir <ecofovka@gmail.com>
2020-07-26 22:29:52 +0200

    re-writed drinksTrack.js with checking drinks in collection

--- Week2/homework/drinksTrack.js
+++ Week2/homework/drinksTrack.js
@@ -1,11 +1,24 @@
 'use strict';
 const drinkTypes = ['cola', 'lemonade', 'water'];
 let drinkTray = [];
-for (let i=0; i<5; i++) {
-    if (i%2 === 0) {
-        drinkTray.push(drinkTypes[0]);
+let currentDrinkIndex = 0;
+let currentDrink;
+drinkTray.push(drinkTypes[0]); 
+while (drinkTray.length < 5) {
+    currentDrink = drinkTypes[currentDrinkIndex];
+    if (drinkTray[drinkTray.length - 1] === currentDrink) {
+        drinkTray.push(currentDrink);
+        currentDrinkIndex++;
     } else {
-        drinkTray.push(drinkTypes.shift());
+        drinkTray.push(currentDrink);
     }
 }
+
+// for (let i=0; i<5; i++) {
+//     if (i%2 === 0) {
+//         drinkTray.push(drinkTypes[0]);
+//     } else {
+//         drinkTray.push(drinkTypes.shift());
+//     }
+// }
 console.log('Hey guys, I brought a ' + drinkTray.join(', ') + '!');
