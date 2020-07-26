bddb62dfd08bc4c2447be02ee90b346dc2d628ad
Vladimir <ecofovka@gmail.com>
2020-07-26 22:17:17 +0200

    re-writed removeComma.js using regex

--- Week2/homework/removeComma.js
+++ Week2/homework/removeComma.js
@@ -1,5 +1,6 @@
 'use strict';
 let myString = 'hello,this,is,a,difficult,to,read,sentence';
 console.log(myString.length);
-myString = (myString.split(',')).join(' ');
+// myString = (myString.split(',')).join(' ');
+myString = myString.replace(/,/g, ' ');
 console.log(myString);
